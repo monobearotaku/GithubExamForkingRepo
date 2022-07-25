cd274c417927080b2186530e215a3c84444ed38a
r.y.nikolaev <r.y.nikolaev@tinkoff.ru>
2022-07-25 12:26:51 +0300

    Init commit

---
+++ internal/math/math.go
@@ -0,0 +1,22 @@
+package math
+
+func Sinus(input float64) (output float64) {
+	// Insert your code here
+	// return math.Sin(input)
+
+	return 0.0
+}
+
+func Cosinus(input float64) (output float64) {
+	// Insert your code here
+	//return math.Cos(input)
+
+	return 0.0
+}
+
+func Tangens(input float64) (output float64) {
+	// Insert your code here
+	//return math.Tan(input)
+
+	return 0.0
+}
